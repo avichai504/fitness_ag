d6a56857cd71296534c1c0e194649fc13b360c9c
Avichai <avichai504@gmail.com>
2023-08-30 21:55:53 +0300

    commit issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
