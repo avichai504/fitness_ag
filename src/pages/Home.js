54ddeec6826d9db0fe749992736857d7c06d4bc7
Avichai <avichai504@gmail.com>
2023-09-22 13:38:12 +0300

    adding to the page the menu card component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
 
-import HeroBanner from "../components/HeroBanner";
 import Welcome from "../components/Welcome";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 import BodyMap from "../components/BodyMap";
+import MenuCards from "../components/MenuCards";
 
 const Home = () => {
   const [bodyPart, setBodyPart] = useState("all");
   const [exercises, setExercises] = useState([]);
   const [inputUser, setInputUser] = useState("");
 
-  // console.log(inputUser) //! I have made it!
-
   return (
     <Box>
-      {/* <HeroBanner /> */}
       <Welcome />
-
+      <MenuCards/>
       <BodyMap />
       <SearchExercises
         setExercises={setExercises}
         bodyPart={bodyPart}
         setBodyPart={setBodyPart}
-        setInputUser={setInputUser} //* This is my addition
+        setInputUser={setInputUser} //* sharing data from user search between components
       />
       <Exercises
         setExercises={setExercises}
         bodyPart={bodyPart}
         exercises={exercises}
-        inputUser={inputUser} //* This is my addition
+        inputUser={inputUser} //* sharing data from user search between components
       />
     </Box>
   );
