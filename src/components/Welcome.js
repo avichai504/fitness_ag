b8616ff9e1c4653a78dc70accb88b511240e16ab
Avichai <avichai504@gmail.com>
2023-09-26 22:09:45 +0300

    adding animation to the welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,29 @@
-import { Stack } from "@mui/material";
+import { useEffect } from 'react';
+import { Stack } from '@mui/material';
+import "../App.css"
 
 const Welcome = () => {
-    return (
-        <Stack id="welcome" padding={20} pl={120} sx={{ color: "wheat", fontSize: "200px" }}>
-            Welcome
-        </Stack>
-    );
+  useEffect(() => {
+    const welcomeElement = document.getElementById('welcome');
+
+    function fadeOutIn() {
+      welcomeElement.style.opacity = 0;
+      setTimeout(() => {
+        welcomeElement.style.opacity = 1;
+      }, 100);
+
+      setTimeout(fadeOutIn, 8000);
+    }
+
+    // Start the animation
+    fadeOutIn();
+  }, []);
+
+  return (
+    <Stack id="welcome" padding={20} pl={120} sx={{ color: 'wheat', fontSize: '200px' }}>
+      Welcome
+    </Stack>
+  );
 };
 
 export default Welcome;
