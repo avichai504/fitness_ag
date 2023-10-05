8c9c9f9ff1728a14aaf93819d783d7b1f1d3fb36
Avichai <avichai504@gmail.com>
2023-10-05 15:20:13 +0300

    one logo to all

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FitnessAndEducationLogo from "../assets/icons/Fitness&Education-LOGO.png";
+
+const Logo = () => {
+  return (
+    <img
+      src={FitnessAndEducationLogo}
+      alt="fitness and education logo"
+      style={{ width: "100px", borderRadius: "50px" }}
+    />
+  );
+};
+
+export default Logo;
