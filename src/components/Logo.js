8e5b08966892771d2565f29cde4a1b9ae663bebb
Avichai <avichai504@gmail.com>
2023-10-06 14:37:29 +0300

    logo needs to be with parameter "width"!

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 import React from "react";
 import FitnessAndEducationLogo from "../assets/icons/Fitness&Education-LOGO.png";
 
-const Logo = () => {
+const Logo = (width) => {
   return (
     <img
       src={FitnessAndEducationLogo}
       alt="fitness and education logo"
-      style={{ width: "80px", borderRadius: "50px" }}
+      style={{ width:"65px", borderRadius: "50px" }}
     />
   );
 };
